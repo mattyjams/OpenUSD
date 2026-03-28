99c65d2e03aca3a86988608875fca55638ee4013
Matt Johnson <mrjz@umich.edu>
2026-03-28 13:10:34 -0400

    bump Ptex to a more recent release
    Ptex switched over from using zlib to using libdeflate instead as of
    version 2.5.0:
    https://github.com/wdas/ptex/commit/99d7a3b30476e5a5b901015f4fd4ded325083a32

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -1456,8 +1456,8 @@ OPENEXR = Dependency("OpenEXR", InstallOpenEXR, "include/OpenEXR/ImfVersion.h")
 ############################################################
 # Ptex
 
-PTEX_URL = "https://github.com/wdas/ptex/archive/refs/tags/v2.4.2.zip"
-PTEX_VERSION = "v2.4.2"
+PTEX_URL = "https://github.com/wdas/ptex/archive/refs/tags/v2.5.4.zip"
+PTEX_VERSION = "v2.5.4"
 
 def InstallPtex(context, force, buildArgs):
     cmakeOptions = [
@@ -2691,7 +2691,7 @@ if context.buildMaterialX:
 
 if context.buildImaging:
     if context.enablePtex:
-        requiredDependencies += [ZLIB, PTEX]
+        requiredDependencies += [DEFLATE, PTEX]
 
     requiredDependencies += [OPENSUBDIV]
 
