5d961474ed402563041b6b7372f91cc345298444
Matt Johnson <mrjz@umich.edu>
2026-03-28 13:10:34 -0400

    bump Alembic to a more recent release

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -1797,7 +1797,7 @@ PYSIDE = PythonDependency("PySide", GetPySideInstructions,
 ############################################################
 # Alembic
 
-ALEMBIC_URL = "https://github.com/alembic/alembic/archive/refs/tags/1.8.5.zip"
+ALEMBIC_URL = "https://github.com/alembic/alembic/archive/refs/tags/1.8.12.zip"
 
 def InstallAlembic(context, force, buildArgs):
     with CurrentWorkingDirectory(DownloadURL(ALEMBIC_URL, context, force)):
