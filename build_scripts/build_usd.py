18afdd53bec090f80dc6ded7d8730ef226c4af15
Matt Johnson <mrjz@umich.edu>
2026-03-28 13:10:33 -0400

    bump oneTBB to a more recent release

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -1097,7 +1097,7 @@ BOOST = Dependency("boost", InstallBoost, *BOOST_VERSION_FILES)
 ############################################################
 # Intel oneTBB
 
-ONETBB_URL = "https://github.com/uxlfoundation/oneTBB/archive/refs/tags/v2021.12.0.zip"
+ONETBB_URL = "https://github.com/uxlfoundation/oneTBB/archive/refs/tags/v2023.1.0.zip"
 
 def InstallOneTBB(context, force, buildArgs):
     with CurrentWorkingDirectory(DownloadURL(ONETBB_URL, context, force)):
